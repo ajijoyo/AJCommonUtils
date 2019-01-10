4e3a76f392ac8132754f362c6984c119d80e0e6f
Ajiejoy-Macbook Pro <self.ajiejoy@gmail.com>
2019-01-10 21:00:59 +0700

    fix smaller when resize;

--- AJCommonUtils.podspec
+++ AJCommonUtils.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
     s.name             = 'AJCommonUtils'
-    s.version          = '0.1.2'
+    s.version          = '0.1.3'
     s.summary          = 'Common Utility on iOS Development'
     s.swift_version    = '4.2'
     
--- AJCommonUtils/Classes/UIImage+Helper.swift
+++ AJCommonUtils/Classes/UIImage+Helper.swift
@@ -56,11 +56,12 @@ public extension UIImage {
     /// - Parameter width: CGFloat value
     /// - Returns: New Image with porpotional size
     func resize(width:CGFloat) -> UIImage?{
-        let scale = width / self.size.width
-        let height = self.size.height * scale
+        if size.width < width { return self }
+        let scale = width / size.width
+        let height = size.height * scale
         
         UIGraphicsBeginImageContextWithOptions(CGSize(width:width, height: height), false, 0);
-        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        draw(in: CGRect(x: 0, y: 0, width: width, height: height))
         let img = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
         return img
